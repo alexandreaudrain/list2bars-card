52350da489cdfacf11d75f5ecd00a20202dbb7fe
Alexandre AUDRAIN <github@rygel.fr>
2020-11-29 14:00:28 +0100

    Init
    Initialisation

---
+++ src/styles.ts
@@ -0,0 +1,235 @@
+import { html } from 'lit-element';
+
+export const styles = html`
+  <style>
+    .warning {
+      display: block;
+      color: black;
+      background-color: #fce588;
+      padding: 8px;
+    }
+    #states {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+    }
+    #states > * {
+      margin-bottom: 8px;
+    }
+    #states > :last-child {
+      margin-top: 0px;
+      margin-bottom: 0px;
+    }
+    #states > :first-child {
+      margin-top: 0px;
+    }
+    ha-card {
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+    }
+    list2bars-row {
+      display: flex;
+      flex-grow: 1;
+    }
+    list2bars-row > div {
+      flex-basis: 100%;
+    }
+    list2bars-row:empty {
+      display: none;
+    }
+    list2bars-card {
+      display: flex;
+      flex-basis: 100%;
+      flex-direction: row;
+      margin-right: 8px;
+    }
+    list2bars-card:last-child {
+      margin-right: 0px;
+    }
+    list2bars-background {
+      cursor: pointer;
+      flex-grow: 1;
+      position: relative;
+    }
+    list2bars-iconbar {
+      color: var(--icon-color, var(--paper-item-icon-color));
+      align-items: center;
+      align-self: center;
+      display: flex;
+      height: 40px;
+      justify-content: center;
+      position: relative;
+      width: 40px;
+    }
+    list2bars-currentbar,
+    list2bars-backgroundbar,
+    list2bars-contentbar,
+    list2bars-targetbar,
+    list2bars-animationbar {
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      border-radius: var(--list2bars-border-radius, var(--ha-card-border-radius));
+    }
+    list2bars-contentbar {
+      align-items: center;
+      color: var(--primary-text-color);
+      display: flex;
+      justify-content: flex-start;
+    }
+    .contentbar-direction-right {
+      flex-direction: row;
+    }
+    .contentbar-direction-up {
+      flex-direction: column;
+    }
+    list2bars-backgroundbar {
+      background: var(--bar-color);
+      filter: brightness(0.5);
+      opacity: 0.25;
+    }
+    list2bars-currentbar {
+      background: linear-gradient(
+        to var(--bar-direction),
+        var(--bar-color) var(--bar-percent),
+        #0000 var(--bar-percent),
+        #0000 var(--bar-percent)
+      );
+    }
+    list2bars-targetbar {
+      background: linear-gradient(
+        to var(--bar-direction),
+        #0000 var(--bar-percent),
+        var(--bar-color) var(--bar-percent),
+        var(--bar-color) var(--bar-target-percent),
+        #0000 var(--bar-target-percent)
+      );
+      display: var(--target-display);
+      filter: brightness(0.66);
+      opacity: 0.33;
+    }
+    list2bars-markerbar {
+      background: var(--bar-color);
+      filter: brightness(0.75);
+      opacity: 50%;
+      position: absolute;
+    }
+    list2bars-animationbar {
+      background-repeat: no-repeat;
+      filter: brightness(0.75);
+      opacity: 0%;
+    }
+    .animationbar-horizontal {
+      background: linear-gradient(to var(--animation-direction), var(--bar-color) 0%, var(--bar-color) 1%, #0000 1%);
+    }
+    .animationbar-vertical {
+      background: linear-gradient(to var(--animation-direction), #0000 0%, #0000 1%, var(--bar-color) 1%);
+    }
+    @keyframes animation-increase {
+      0% {
+        opacity: 50%;
+        background-size: var(--bar-percent) 100%;
+      }
+      100% {
+        opacity: 0%;
+        background-size: 10000% 100%;
+      }
+    }
+    @keyframes animation-decrease {
+      0% {
+        opacity: 0%;
+        background-size: 10000%;
+      }
+      100% {
+        opacity: 50%;
+        background-size: var(--bar-percent);
+      }
+    }
+    @keyframes animation-increase-vertical {
+      0% {
+        opacity: 50%;
+        background-size: 100% var(--bar-percent);
+      }
+      100% {
+        background-size: 100% 0%;
+        opacity: 0%;
+      }
+    }
+    @keyframes animation-decrease-vertical {
+      0% {
+        background-size: 100% 100%;
+        opacity: 0%;
+      }
+      100% {
+        opacity: 50%;
+        background-size: 100% var(--bar-percent);
+      }
+    }
+    list2bars-indicator {
+      align-self: center;
+      color: var(--bar-color);
+      filter: brightness(0.75);
+      height: 16px;
+      width: 16px;
+      position: relative;
+      text-align: center;
+    }
+    .indicator-direction-right {
+      margin-right: -16px;
+      left: -6px;
+    }
+    .indicator-direction-up {
+      margin: 4px;
+    }
+    list2bars-name {
+      align-items: center;
+      align-self: center;
+      justify-content: center;
+      margin: 4px;
+      overflow: hidden;
+      position: relative;
+      text-align: left;
+      text-overflow: ellipsis;
+    }
+    .name-outside {
+      margin-left: 16px;
+    }
+    list2bars-value,
+    list2bars-min,
+    list2bars-max,
+    list2bars-divider {
+      align-self: center;
+      position: relative;
+    }
+    list2bars-min,
+    list2bars-max,
+    list2bars-divider {
+      font-size: 10px;
+      margin: 2px;
+      opacity: 0.5;
+    }
+    .min-direction-up {
+      margin-top: auto;
+    }
+    .min-direction-right {
+      margin-left: auto;
+    }
+    list2bars-divider {
+      margin-left: 0px;
+      margin-right: 0px;
+    }
+    list2bars-value {
+      white-space: nowrap;
+      margin: 4px;
+    }
+    .value-direction-right {
+      margin-left: auto;
+    }
+    .value-direction-up {
+      margin-top: auto;
+    }
+  </style>
+`;
+
+export default styles;
